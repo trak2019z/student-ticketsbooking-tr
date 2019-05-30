251905d37ea306661793e860e444f5d0d3f9709c
Łukasz Szęszoł <lusz@softwaremind.com>
2019-05-30 18:32:35 +0200

    BKN/004 Added new appsettings

--- Microservices.Booking/Microservices.Booking/Controllers/ValuesController.cs
+++ Microservices.Booking/Microservices.Booking/Controllers/ValuesController.cs
@@ -1,20 +1,35 @@
+<<<<<<< Updated upstream:Microservices.Booking/Microservices.Booking/Controllers/ValuesController.cs
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+=======
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microservices.Booking.BussinessLogic.Models;
+using Microservices.Booking.BussinessLogic.Services.MoviePremieresReader;
+using Microservices.Common.Dispatchers;
+using Microservices.Common.Mvc.Middleware;
+>>>>>>> Stashed changes:Microservices.Booking/Microservices.Booking/Web/Controllers/ValuesController.cs
 using Microsoft.AspNetCore.Mvc;
 
 namespace Microservices.Booking.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly IMoviePremieresReaderService<TheMovieDbMovie> _moviePremieresReaderService;
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult<IEnumerable<string>>> Get()
         {
+<<<<<<< Updated upstream:Microservices.Booking/Microservices.Booking/Controllers/ValuesController.cs
             return new string[] { "value1", "value2" };
+=======
+            var a = await _moviePremieresReaderService.GetMoviesAsync();
+            return new[] { "value1", "value2" };
+>>>>>>> Stashed changes:Microservices.Booking/Microservices.Booking/Web/Controllers/ValuesController.cs
         }
 
         // GET api/values/5
@@ -41,5 +56,13 @@ namespace Microservices.Booking.Controllers
         public void Delete(int id)
         {
         }
+<<<<<<< Updated upstream:Microservices.Booking/Microservices.Booking/Controllers/ValuesController.cs
+=======
+
+        public ValuesController(IDispatcher dispatcher, IMoviePremieresReaderService<TheMovieDbMovie> moviePremieresReaderService) : base(dispatcher)
+        {
+            _moviePremieresReaderService = moviePremieresReaderService;
+        }
+>>>>>>> Stashed changes:Microservices.Booking/Microservices.Booking/Web/Controllers/ValuesController.cs
     }
 }
