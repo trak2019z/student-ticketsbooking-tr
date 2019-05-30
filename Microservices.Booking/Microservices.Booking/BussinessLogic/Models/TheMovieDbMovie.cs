d169d837c02078305747d505b2b000f9188c60da
Łukasz Szęszoł <lusz@softwaremind.com>
2019-05-30 18:26:21 +0200

    BKN/004 Added model forTheMovieDb API movies

---
+++ Microservices.Booking/Microservices.Booking/BussinessLogic/Models/TheMovieDbMovie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservices.Booking.BussinessLogic.Models
+{
+    public class TheMovieDbMovie
+    {
+    }
+    public class Result
+    {
+        public int vote_count { get; set; }
+        public int id { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public string title { get; set; }
+        public double popularity { get; set; }
+        public string poster_path { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public List<int> genre_ids { get; set; }
+        public string backdrop_path { get; set; }
+        public bool adult { get; set; }
+        public string overview { get; set; }
+        public string release_date { get; set; }
+    }
+
+    public class Dates
+    {
+        public string maximum { get; set; }
+        public string minimum { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Result> results { get; set; }
+        public int page { get; set; }
+        public int total_results { get; set; }
+        public Dates dates { get; set; }
+        public int total_pages { get; set; }
+    }
+}
