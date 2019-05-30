8d29a68a435725388946567c27c0a07003c4895e
Łukasz Szęszoł <lusz@softwaremind.com>
2019-05-30 18:27:25 +0200

    BKN/004 Added service for reading movies from TheMovieDb

---
+++ Microservices.Booking/Microservices.Booking/BussinessLogic/Services/MoviePremieresReader/MoviePremieresTheMovieDbMovieReaderService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Flurl.Http.Configuration;
+using Microservices.Booking.BussinessLogic.Models;
+using Microsoft.Extensions.Options;
+
+namespace Microservices.Booking.BussinessLogic.Services.MoviePremieresReader
+{
+    internal sealed class MoviePremieresTheMovieDbMovieReaderService : IMoviePremieresReaderService<TheMovieDbMovie>
+    {
+        private readonly TheMovieDbApiSettings _apiSettings;
+        private readonly IFlurlClientFactory _httpClient;
+
+
+        public MoviePremieresTheMovieDbMovieReaderService(IFlurlClientFactory httpClient,
+            IOptions<TheMovieDbApiSettings> options)
+        {
+            _httpClient = httpClient;
+            _apiSettings = options.Value;
+        }
+
+        public async Task<IEnumerable<TheMovieDbMovie>> GetMoviesAsync()
+        {
+            var httpClient = _httpClient.Get(_apiSettings.Url);
+            var response = await httpClient
+                .Request(_apiSettings.Url)
+                .SetQueryParam("Apikey", _apiSettings.ApiKey)
+                .GetJsonAsync<IEnumerable<TheMovieDbMovie>>();
+
+            return response;
+        }
+    }
+}
---
+++ Microservices.Booking/Microservices.Booking/BussinessLogic/Services/MoviePremieresReader/Settings/TheMovieDbApiSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Microservices.Booking.BussinessLogic.Services.MoviePremieresReader
+{
+    internal sealed class TheMovieDbApiSettings
+    {
+        public TheMovieDbApiSettings(string url, string apiKey)
+        {
+            Url = url;
+            ApiKey = apiKey;
+        }
+
+        public TheMovieDbApiSettings()
+        {
+            
+        }
+
+        public string Url { get; }
+        public string ApiKey { get; }
+    }
+}
