52ad9c4306947d2a37b687a21076e4b1351690ca
Łukasz Szęszoł <lusz@softwaremind.com>
2019-06-18 22:52:56 +0200

    BKN/002 Added settings for cinemas

---
+++ Microservices.Booking/Microservices.Booking/BussinessLogic/Settings/CinemaSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Microservices.Booking.BussinessLogic.Settings
+{
+    public class CinemasSettings
+    {
+        public List<CinemaSettings> Cinemas { get; set; }
+    }
+    public class CinemaSettings
+    {
+        public string City { get; set; }
+        public uint Rows { get; set; }
+        public uint Columns { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public List<string> MovieShowHours { get; set; }
+    }
+}
